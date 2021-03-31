bad17a825236240f04d24f956d551ce1784b4195
Francesco Cappetti <f.cappetti.05@gmail.com>
2021-04-01 00:31:53 +0200

    Fixed import

--- pytyping/main.py
+++ pytyping/main.py
@@ -1,6 +1,6 @@
 import curses
 import time
-from quotes import get_quote
+from pytyping.quotes import get_quote
 
 
 VALIDS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
